2bc3bd5d1c274814255464473cb74d061f552f23
RomeoNotaLoka <eldominimaster@gmail.com>
2026-02-25 20:56:17 -0500

    chore: fix formatting

--- site/src/script.js
+++ site/src/script.js
@@ -1,14 +1,16 @@
 async function updateVisitorCount() {
   try {
-    const response = await fetch('https://p62ky3oajf.execute-api.us-east-1.amazonaws.com');
+    const response = await fetch(
+      "https://p62ky3oajf.execute-api.us-east-1.amazonaws.com",
+    );
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    document.getElementById('visitor-count').textContent = data.count;
+    document.getElementById("visitor-count").textContent = data.count;
   } catch (error) {
-    console.error('Error fetching visitor count:', error);
-    document.getElementById('visitor-count').textContent = 'N/A';
+    console.error("Error fetching visitor count:", error);
+    document.getElementById("visitor-count").textContent = "N/A";
   }
 }
 
